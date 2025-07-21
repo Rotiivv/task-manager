eba96458169f5c7544db7d9990d8b30f346ffc9a
Vitor <vitormendesesilva1@gmail.com>
2025-07-21 15:42:58 -0300

    feat: add size and terciary variant to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-const Button = ({ children, variant = "primary", ...rest }) => {
+const Button = ({ children, variant = "primary", size = "small", ...rest }) => {
   const getVariantClass = () => {
     if (variant === "primary") {
       return "bg-[#00ADB5] text-white";
@@ -7,11 +7,25 @@ const Button = ({ children, variant = "primary", ...rest }) => {
     if (variant === "secundary") {
       return "bg-transparent text-[#818181]";
     }
+
+    if (variant === "terciary") {
+      return "bg-[#EEE] text-[#35383E]";
+    }
+  };
+
+  const getSizeClass = () => {
+    if (size === "small") {
+      return "py-1 text-xs";
+    }
+
+    if (size === "large") {
+      return "py-2 text-sm w-full justify-center";
+    }
   };
   return (
     <button
       {...rest}
-      className={`${getVariantClass()} hover:opacity-75 transition rounded-md px-3 py-1 text-xs flex gap-1 items-center`}
+      className={`${getVariantClass()} hover:opacity-75 transition rounded-md px-3 flex gap-1 items-center font-semibold ${getSizeClass()}`}
     >
       {children}
     </button>
