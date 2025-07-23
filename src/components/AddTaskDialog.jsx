60058d38dfa58d4ee7e51827e6bd7486c49297f1
Vitor <vitormendesesilva1@gmail.com>
2025-07-23 18:15:10 -0300

    feat: add TimeSelect component

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 import { CSSTransition } from "react-transition-group";
 import { useRef } from "react";
 import "./AddTaskDialog.css";
-import InputLabel from "./InputLabel";
+import TimeSelect from "./TimeSelect";
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
   const nodeRef = useRef(null);
@@ -40,18 +40,7 @@ const AddTaskDialog = ({ isOpen, handleClose }) => {
                   id="title"
                 />
 
-                <div className="flex flex-col gap-1">
-                  <InputLabel>Horario</InputLabel>
-
-                  <select
-                    className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder:text-sm placeholder:text-[#9A9C9F] focus:outline focus:outline-[#00ADB5] bg-white"
-                    id="time"
-                  >
-                    <option value="morning">Manha</option>
-                    <option value="afternoon">Tarde</option>
-                    <option value="moon">Noite</option>
-                  </select>
-                </div>
+                <TimeSelect />
 
                 <Input
                   label="Descricao"
---
+++ src/components/TimeSelect.jsx
@@ -0,0 +1,20 @@
+import InputLabel from "./InputLabel";
+
+const TimeSelect = () => {
+  return (
+    <div className="flex flex-col gap-1">
+      <InputLabel>Horario</InputLabel>
+
+      <select
+        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder:text-sm placeholder:text-[#9A9C9F] focus:outline focus:outline-[#00ADB5] bg-white"
+        id="time"
+      >
+        <option value="morning">Manha</option>
+        <option value="afternoon">Tarde</option>
+        <option value="moon">Noite</option>
+      </select>
+    </div>
+  );
+};
+
+export default TimeSelect;
